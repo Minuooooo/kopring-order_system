333a5c254a10b74ebbf249f0891e0b62ec65cf91
Minuooooo <mmw7741@gmail.com>
2023-06-14 18:00:43 +0900

    [ feat ] Add yml files

--- src/main/kotlin/order/system/SystemApplication.kt
+++ src/main/kotlin/order/system/SystemApplication.kt
@@ -2,8 +2,10 @@ package order.system
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.PropertySource
 
 @SpringBootApplication
+@PropertySource("classpath:secure.properties")
 class SystemApplication
 
 fun main(args: Array<String>) {
