92cd18ea798952851aaa0fcebd5ab3a7699c3dd0
Minuooooo <mmw7741@gmail.com>
2023-06-14 17:16:37 +0900

    [ env ] Create project

---
+++ system/src/test/kotlin/order/system/SystemApplicationTests.kt
@@ -0,0 +1,13 @@
+package order.system
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SystemApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
