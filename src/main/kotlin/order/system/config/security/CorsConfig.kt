2a5708331d8c36aa2c14658827fbf53c5b21e048
Minuooooo <mmw7741@gmail.com>
2023-06-27 17:34:14 +0900

    [ feat ] Add security filter

---
+++ src/main/kotlin/order/system/config/security/CorsConfig.kt
@@ -0,0 +1,26 @@
+package order.system.config.security
+
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
+import org.springframework.web.filter.CorsFilter
+
+@Configuration
+class CorsConfig {
+
+    @Bean
+    fun corsFilter(): CorsFilter {
+        val source = UrlBasedCorsConfigurationSource()
+        val config = CorsConfiguration()
+
+        config.allowCredentials = true
+        config.addAllowedHeader("*")  // 요청 url
+        config.addAllowedHeader("*")  // 요청 header
+        config.addAllowedMethod("*")  // 요청 HTTP method
+
+        source.registerCorsConfiguration("/**", config)
+        return CorsFilter(source)
+    }
+}
---
+++ src/main/kotlin/order/system/config/security/SecurityConfig.kt
@@ -0,0 +1,60 @@
+package order.system.config.security
+
+import order.system.config.jwt.JwtAccessDeniedHandler
+import order.system.config.jwt.JwtAuthenticationEntryPoint
+import order.system.config.jwt.JwtFilter
+import order.system.config.jwt.JwtProvider
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+import org.springframework.web.filter.CorsFilter
+import kotlin.jvm.Throws
+
+@Configuration
+class SecurityConfig(
+        private val jwtProvider: JwtProvider,
+        private val corsFilter: CorsFilter,
+        private val jwtAuthenticationEntryPoint: JwtAuthenticationEntryPoint,
+        private val jwtAccessDeniedHandler: JwtAccessDeniedHandler
+) {
+
+    @Bean
+    fun passwordEncoder(): PasswordEncoder = BCryptPasswordEncoder()
+
+    @Bean
+    @Throws(Exception::class)
+    protected fun config(http: HttpSecurity): SecurityFilterChain =
+            http
+                    .csrf {
+                        it
+                                .disable()
+                                .addFilterBefore(corsFilter, UsernamePasswordAuthenticationFilter::class.java)
+                    }
+
+                    .sessionManagement {
+                        it.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                    }
+
+                    .addFilterBefore(JwtFilter(jwtProvider), UsernamePasswordAuthenticationFilter::class.java)
+
+                    .exceptionHandling {
+                        it
+                                .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                                .accessDeniedHandler(jwtAccessDeniedHandler)
+                    }
+
+                    .authorizeHttpRequests{
+                        it
+                                .antMatchers("/swagger-ui/**", "/api-docs/**", "/v1/**", "/api")  // 인증하지 않을 url
+                                .permitAll()
+                                .anyRequest()
+                                .authenticated()
+                    }
+
+                    .build()
+}
