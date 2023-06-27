cb3cd5273028194e8a948c6d5551cf69b6a4025a
Minuooooo <mmw7741@gmail.com>
2023-06-27 16:49:34 +0900

    [ feat ] Config redis

---
+++ src/main/kotlin/order/system/config/redis/RedisService.kt
@@ -0,0 +1,13 @@
+package order.system.config.redis
+
+import org.springframework.data.redis.core.RedisTemplate
+import org.springframework.stereotype.Service
+import java.time.Duration
+
+@Service
+class RedisService(private val redisTemplate: RedisTemplate<String, String>) {
+
+    fun setValues(key: String, data: String, duration: Duration) = redisTemplate.opsForValue().set(key, data, duration)
+    fun getValues(key: String) = redisTemplate.opsForValue()[key]
+    fun deleteValues(key: String) = redisTemplate.delete(key)
+}
