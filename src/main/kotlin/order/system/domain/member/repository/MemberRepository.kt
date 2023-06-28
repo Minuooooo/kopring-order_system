416626098fbc3741ee368fe5f402780a39c5ea2f
Minuooooo <mmw7741@gmail.com>
2023-06-28 13:06:32 +0900

    [ feat ] Member repository

---
+++ src/main/kotlin/order/system/domain/member/repository/MemberRepository.kt
@@ -0,0 +1,9 @@
+package order.system.domain.member.repository
+
+import order.system.domain.member.entity.Member
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface MemberRepository : JpaRepository<Member, Long> {
+    fun findByUsername(username: String): Member?
+    fun existsByUsername(username: String): Boolean
+}
