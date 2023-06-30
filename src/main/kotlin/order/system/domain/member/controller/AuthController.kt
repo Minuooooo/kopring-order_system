1320d68fa8faa704711086ed7351b20c5a34442a
Minuooooo <mmw7741@gmail.com>
2023-06-30 15:09:24 +0900

    [ feat ] Token reissue API

--- src/main/kotlin/order/system/domain/member/controller/AuthController.kt
+++ src/main/kotlin/order/system/domain/member/controller/AuthController.kt
@@ -6,6 +6,7 @@ import order.system.config.oauth.param.KakaoLoginParams
 import order.system.config.oauth.param.NaverLoginParams
 import order.system.domain.member.dto.sign.SignInRequestDto
 import order.system.domain.member.dto.sign.SignUpRequestDto
+import order.system.domain.member.dto.sign.TokenRequestDto
 import order.system.domain.member.service.AuthService
 import order.system.domain.member.service.MemberService
 import order.system.response.Response
@@ -34,7 +35,7 @@ class AuthController(
     fun getKakaoEmail(@RequestBody kakaoLoginParams: KakaoLoginParams): Response =
             success(SuccessMessage.SUCCESS_TO_GET_EMAIL, authService.getUsernameBySocial(kakaoLoginParams))
 
-    @Operation(summary = "Get naver email API", description = "please sign in aver.")
+    @Operation(summary = "Get naver email API", description = "please sign in naver.")
     @ResponseStatus(OK)
     @GetMapping("/sign-up/email/naver")
     fun getNaverEmail(@RequestBody naverLoginParams: NaverLoginParams): Response =
@@ -51,23 +52,20 @@ class AuthController(
     @Operation(summary = "Sign in API", description = "put your sign in info.")
     @ResponseStatus(OK)
     @PostMapping("/sign-in")
-    fun signIn(@Valid @RequestBody signInRequestDto: SignInRequestDto): Response {
-        return success(SuccessMessage.SUCCESS_TO_SIGN_IN, authService.signIn(signInRequestDto))
-    }
+    fun signIn(@Valid @RequestBody signInRequestDto: SignInRequestDto): Response =
+            success(SuccessMessage.SUCCESS_TO_SIGN_IN, authService.signIn(signInRequestDto))
 
     @Operation(summary = "Sign in kakao API", description = "please sign in kakao.")
     @ResponseStatus(OK)
     @PostMapping("/sign-in/kakao")
-    fun signInWithKakao(@RequestBody kakaoLoginParams: KakaoLoginParams): Response {
-        return success(SuccessMessage.SUCCESS_TO_SIGN_IN, authService.signInWithSocial(kakaoLoginParams))
-    }
+    fun signInWithKakao(@RequestBody kakaoLoginParams: KakaoLoginParams): Response =
+            success(SuccessMessage.SUCCESS_TO_SIGN_IN, authService.signInWithSocial(kakaoLoginParams))
 
     @Operation(summary = "Sign in naver API", description = "please sign in naver.")
     @ResponseStatus(OK)
     @PostMapping("/sign-in/naver")
-    fun signInWithNaver(@RequestBody naverLoginParams: NaverLoginParams): Response {
-        return success(SuccessMessage.SUCCESS_TO_SIGN_IN, authService.signInWithSocial(naverLoginParams))
-    }
+    fun signInWithNaver(@RequestBody naverLoginParams: NaverLoginParams): Response =
+            success(SuccessMessage.SUCCESS_TO_SIGN_IN, authService.signInWithSocial(naverLoginParams))
 
     @Operation(summary = "Logout API", description = "please logout.")
     @ResponseStatus(OK)
@@ -77,5 +75,9 @@ class AuthController(
         return success(SuccessMessage.SUCCESS_TO_LOGOUT)
     }
 
-
+    @Operation(summary = "Reissue API", description = "put your token info.")
+    @ResponseStatus(OK)
+    @PostMapping("/reissue")
+    fun reissue(@RequestBody tokenRequestDto: TokenRequestDto): Response =
+            success(SuccessMessage.SUCCESS_TO_REISSUE, authService.reissue(tokenRequestDto))
 }
