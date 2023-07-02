fc8b67b2591874c70d1db5d6fe79e642f1340efe
Minuooooo <mmw7741@gmail.com>
2023-07-02 11:00:07 +0900

    [ fix ] Apply swagger annotation to constructor property

--- src/main/kotlin/order/system/domain/member/dto/sign/Sign.kt
+++ src/main/kotlin/order/system/domain/member/dto/sign/Sign.kt
@@ -5,41 +5,40 @@ import io.swagger.v3.oas.annotations.media.Schema
 import order.system.domain.member.entity.Address
 import order.system.domain.member.entity.Authority
 import order.system.domain.member.entity.Member
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.util.StringUtils
-import java.util.UUID
+import java.util.*
 import javax.validation.constraints.NotBlank
 import javax.validation.constraints.Pattern
 import javax.validation.constraints.Size
 
 data class SignUpRequestDto(
-        @NotBlank(message = "이메일을 입력해주세요.")
-        @Pattern(regexp = "^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$", message = "이메일은 @를 포함해야 합니다.") // 이메일 형식
-        @Schema(description = "이메일", defaultValue = "test@gmail.com")
+        @field:NotBlank(message = "이메일을 입력해주세요.")
+        @field:Pattern(regexp = "^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$", message = "이메일은 @를 포함해야 합니다.") // 이메일 형식
+        @field:Schema(description = "이메일", defaultValue = "test@gmail.com")
         val username: String,
 
-        @NotBlank(message = "비밀번호를 입력해주세요.")
-        @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,}$", message = "비밀번호는 최소 8자리이면서 1개 이상의 알파벳, 숫자, 특수문자를 포함해야합니다.") // 비밀번호 형식
-        @Schema(description = "비밀번호", defaultValue = "test1234!")
+        @field:NotBlank(message = "비밀번호를 입력해주세요.")
+        @field:Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,}$", message = "비밀번호는 최소 8자리이면서 1개 이상의 알파벳, 숫자, 특수문자를 포함해야합니다.") // 비밀번호 형식
+        @field:Schema(description = "비밀번호", defaultValue = "test1234!")
         val password: String,
 
-        @NotBlank(message = "사용자 이름을 입력해주세요.")
-        @Size(min = 2, message = "사용자 이름이 너무 짧습니다.")
-        @Schema(description = "이름", defaultValue = "홍길동")
+        @field:NotBlank(message = "사용자 이름을 입력해주세요.")
+        @field:Size(min = 2, message = "사용자 이름이 너무 짧습니다.")
+        @field:Schema(description = "이름", defaultValue = "홍길동")
         val name: String,
 
-        @NotBlank(message = "시,도,군을 입력해주세요.")
-        @Schema(description = "시,도,군", defaultValue = "경기도")
+        @field:NotBlank(message = "시,도,군을 입력해주세요.")
+        @field:Schema(description = "시,도,군", defaultValue = "경기도")
         val city: String,
 
-        @NotBlank(message = "도로명을 입력해주세요.")
-        @Schema(description = "도로명", defaultValue = "고양이 네로")
+        @field:NotBlank(message = "도로명을 입력해주세요.")
+        @field:Schema(description = "도로명", defaultValue = "고양이 네로")
         val street: String,
 
-        @NotBlank(message = "우편번호를 입력해주세요.")
-        @Pattern(regexp = "^\\d{5}$", message = "우편번호는 5자리이어야 합니다.") // 우편번호 형식
-        @Schema(description = "우편번호", defaultValue = "11111")
+        @field:NotBlank(message = "우편번호를 입력해주세요.")
+        @field:Pattern(regexp = "^\\d{5}$", message = "우편번호는 5자리이어야 합니다.") // 우편번호 형식
+        @field:Schema(description = "우편번호", defaultValue = "11111")
         val zipcode: String
 ) {
     fun toEntity(passwordEncoder: PasswordEncoder): Member =
@@ -61,12 +60,12 @@ data class SignUpRequestDto(
 }
 
 data class SignInRequestDto(
-        @NotBlank(message = "이메일을 입력해주세요.")
-        @Schema(description = "이메일", defaultValue = "test@gmail.com")
+        @field:NotBlank(message = "이메일을 입력해주세요.")
+        @field:Schema(description = "이메일", defaultValue = "test@gmail.com")
         val username: String,
 
-        @NotBlank(message = "비밀번호를 입력해주세요.")
-        @Schema(description = "비밀번호", defaultValue = "test1234!")
+        @field:NotBlank(message = "비밀번호를 입력해주세요.")
+        @field:Schema(description = "비밀번호", defaultValue = "test1234!")
         val password: String
 )
 
--- src/main/kotlin/order/system/response/Response.kt
+++ src/main/kotlin/order/system/response/Response.kt
@@ -7,13 +7,13 @@ import org.springframework.http.HttpStatus
 import org.springframework.http.HttpStatus.*
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
-@JsonPropertyOrder("isSuccess", "code", "message", "result")
+@JsonPropertyOrder("isSuccess", "code", "message", "data")
 @Schema(description = "This is response message from server")
 data class Response(  // TODO code, data 만을 활용해 custom response 구현
-        private val isSuccess: Boolean,
-        private val code: Int,
-        private val message: String,
-        private val data: Any?
+        val isSuccess: Boolean,
+        val code: Int,
+        val message: String,
+        val data: Any?
 ) {
     companion object {
         fun success(message: String): Response = Response(true, OK.value(), message, null);
